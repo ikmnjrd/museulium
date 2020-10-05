6a921a5558edbaa0af68dea41f771008a76f888d
ike <40803799+ikmnjrd@users.noreply.github.com>
2020-10-06 00:56:23 +0900

    add Modal of Odaimoku

---
+++ Readme.md
@@ -0,0 +1,9 @@
+`git clone  https://github.com/ikmnjrd/museulium.git`
+`docker-compose build`
+`docker-compose run web sh`
+web内にて
+cdでsrcのあるディレクトリに移動
+`npm install react-scripts`
+`npm install react-modal`
+退出
+`docker-compose up -d`
--- museulium/src/CallMet.js
+++ museulium/src/CallMet.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import Modal from 'react-modal';
 
 const met_json = require('./metId.json');
 const metObjs = met_json.metObjIds;
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root')
+
 function CallMet() {
+  // API part
   const [error, setError] = React.useState(null);
   const [isLoaded, setIsLoaded] = React.useState(false);
   const [items, setItems] = React.useState([]);
 
+  // Modal part
+  var subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
 
+  // API part
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
@@ -32,6 +52,20 @@ function CallMet() {
       )
   }, [])
 
+  // Modal part
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
@@ -39,7 +73,20 @@ function CallMet() {
   } else {
     return (
       <div>
-        <img src={items.primaryImage} alt="met_pic" width="500px" height="500px" />
+        <button onClick={openModal} className="imgbutton">
+          <img src={items.primaryImage} alt="met_pic" width="100px" height="100px" />
+        </button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Odaimoku-Modal"
+        >
+          <h2 ref={_subtitle => (subtitle = _subtitle)}>お題</h2>
+          <img src={items.primaryImage} alt="met_pic" width="500px" height="500px" />
+          <button onClick={closeModal}>close</button>
+        </Modal>
       </div>  
     );
   }
