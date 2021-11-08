494600c0a8171bd40260c4396fbc71375f31419e
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-11-09 01:26:18 +0900

    sort images

--- museulium/src/StartPage.js
+++ museulium/src/StartPage.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid'
 import Typography  from '@material-ui/core/Typography';
-import TopImage from './bijyutsu_paint_man2.png';
+import TopImage from './img/bijyutsu_paint_man2.png';
 import { makeStyles } from '@material-ui/core/styles';
 import GridListOfPieces from './GridListOfPieces';
 
